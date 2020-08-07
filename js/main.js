5edd23bb1fd0f0ba6a60fbcd549d48ef0e6dae0c
paulinavaira <paulinavaira@users.noreply.github.com>
2020-08-07 09:35:12 -0300

    Second commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+//Loader
+
+  $(window).on("load",function(){
+    $(".loader").fadeOut(2500);
+  });
+
+//No recargar página
+
+  $(".btnContact").submit(function(e) {
+    e.preventDefault();
+});
+
+//ALERT Contact
+
+$('.btnContact').click(function(){  
+  swal({
+    title:"¿Estás seguro?",
+    text: "Corroborá que los datos sean correctos",
+    buttons: {
+           cancel: true,
+           confirm: "Aceptar"
+             }
+  }).then( val => {
+    if(val)  {
+            swal({
+                 title: "¡Listo!",
+                 text: "Tus datos han sido enviados. Te contactaremos en breve.",
+                 icon: "success"
+                 });
+             }
+  });
+});
+
+//Pido info a la API
+fetch('https://apipetshop.herokuapp.com/api/articulos')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    var data = myJson.response
+    miPrograma(data);
+  });
+
+//Información
+
+function miPrograma(data){
+    var app = new Vue({
+        el: '#app',
+        data:{
+            toys : [],
+            pharmacy : [],
+            stock : [],
+}
+});
+
+//Obtengo los datos para el catálogo 'Farmacia' y 'Juguetes'
+var toys = data.filter(function(toys) {
+    return toys.tipo == 'Juguete';
+});
+
+app.toys = toys;
+
+var pharmacy = data.filter(function(pharmacy) {
+    return pharmacy.tipo == 'Medicamento';
+});
+
+app.pharmacy = pharmacy;
+
+//Obtengo el stock menor a 5
+var stock5 = []
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].stock <= 5){ 
+        stock5.push('Últimas unidades')
+      }   else {
+        stock5.push(data[i].stock)
+      }
+    }
+}
+stock.push(stock5)
